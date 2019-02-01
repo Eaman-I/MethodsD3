08eea458fdcf599ef4b7323f5fae4caa45438ee1
admin <admin@9222OVERLOOK>
2019-01-31 22:58:15 -0500

    V5

--- MethodsD3/Program.cs
+++ MethodsD3/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ Author: Eaman Iranmanesh
+ Date: 1/31/2019
+ Comments: This C# app code demonstrates the use of methods. It does so by repeating a users name back to them.
+ */
+using System;
 
 namespace MethodsD3
 {
@@ -6,7 +11,32 @@ namespace MethodsD3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hell0! Please enter your name."); // user asked to enter name
+
+            try // try catch block to verfiy user input
+            {
+                // variable declared to gather input
+                string input = Console.ReadLine();
+
+                // execute HelloUser method to read back name.
+                HelloUser(input);
+            }
+            catch
+            {
+
+            }
+
+        } // End of Main
+
+        // Method used to repeat users name back to them.
+        private static void HelloUser(string input)
+        {
+            // Sets user input to string name
+            string name = input;
+
+            Console.WriteLine("Hello " + name + "!"); // greets user by name
+            Console.WriteLine("Press any key to exit the application."); // program end message. asks user to exit or not.
+            Console.ReadKey(true);
         }
     }
 }
